7f1bd900fa656eb7c855f0534dd3c952270994b4
Brian Okanga <briannokanga@gmail.com>
2020-07-17 23:51:55 +0300

    Component, props and propTypes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { Component } from "react"
-import "./App.css"
+import React, { Component } from 'react';
+import Navbar from './components/layouts/Navbar';
+import UserItem from './components/users/UserItem';
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <h1>Hello from react</h1>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className='App'>
+				<Navbar title='Github Finder' icon='fab fa-github' />
+				<UserItem />
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class userItem extends Component {
+	render() {
+		return <div>UserItem</div>;
+	}
+}
+
+export default userItem;
