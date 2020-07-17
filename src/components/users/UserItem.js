d0d338cddc6bded981abd331c8a5e14a647d58e3
Brian Okanga <briannokanga@gmail.com>
2020-07-18 01:15:55 +0300

    added state

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,9 +1,39 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class userItem extends Component {
-	render() {
-		return <div>UserItem</div>;
-	}
+  //adding state
+  state = {
+    id: 'id',
+    login: 'mojombo',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+    home_url: 'https://github.com/mojombo',
+  };
+
+  render () {
+    //   destructuring
+    const {login, avatar_url, home_url} = this.state;
+
+    return (
+      <div className="card text-center">
+        <img
+          src={avatar_url}
+          className="round-img"
+          alt=""
+          style={{width: '60px'}}
+        />
+        <h3>{login}</h3>
+        <div>
+          <a
+            href={home_url}
+            className="btn btn-dark btn-sm my-1"
+            style={{borderRadius: '5px'}}
+          >
+            More
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default userItem;
