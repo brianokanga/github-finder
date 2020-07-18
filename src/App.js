af040a53d864f7ccc03aa6bc2165c19544d78609
Brian Okanga <briannokanga@gmail.com>
2020-07-18 03:18:15 +0300

    converted classes to functional components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Navbar from './components/layouts/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
-	render() {
-		return (
-			<div className='App'>
-				<Navbar title='Github Finder' icon='fab fa-github' />
-				<UserItem />
-			</div>
-		);
-	}
+  render () {
+    return (
+      <div className="App">
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <div className="container">
+          <Users />
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,39 +1,32 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class userItem extends Component {
-  //adding state
-  state = {
-    id: 'id',
-    login: 'mojombo',
-    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-    home_url: 'https://github.com/mojombo',
-  };
-
-  render () {
-    //   destructuring
-    const {login, avatar_url, home_url} = this.state;
-
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          className="round-img"
-          alt=""
-          style={{width: '60px'}}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a
-            href={home_url}
-            className="btn btn-dark btn-sm my-1"
-            style={{borderRadius: '5px'}}
-          >
-            More
-          </a>
-        </div>
+//   destructuring
+const userItem = ({user: {login, avatar_url, home_url}}) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        className="round-img"
+        alt=""
+        style={{width: '60px'}}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a
+          href={home_url}
+          className="btn btn-dark btn-sm my-1"
+          style={{borderRadius: '5px'}}
+        >
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+userItem.prototype = {
+  user: PropTypes.object.isRequired,
+};
 
 export default userItem;
